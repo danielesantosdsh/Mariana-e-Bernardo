c4f7887886fa53013a30b2a645430a63995df2d6
danielesantosdsh <danielesantosdsh@gmail.com>
2026-08-04 14:26:56 -0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,624 @@
+const CONFIG = {
+
+    pix: "exemplo@lista.com"
+
+};
+
+
+const PRESENTES = [
+
+    // =====================
+    // COZINHA - 40 ITENS
+    // =====================
+
+    {
+        categoria: "cozinha",
+        nome: "Jogo de Copos",
+        valor: 35
+    },
+
+    {
+        categoria: "cozinha",
+        nome: "Pano de Prato",
+        valor: 40
+    },
+
+    {
+        categoria: "cozinha",
+        nome: "Abridor de Garrafas",
+        valor: 45
+    },
+
+    {
+        categoria: "cozinha",
+        nome: "Descanso de Panela",
+        valor: 50
+    },
+
+    {
+        categoria: "cozinha",
+        nome: "Ralador Multifuncional",
+        valor: 55
+    },
+
+    {
+        categoria: "cozinha",
+        nome: "Forma de Bolo",
+        valor: 60
+    },
+
+    {
+        categoria: "cozinha",
+        nome: "Escorredor de Talheres",
+        valor: 70
+    },
+
+    {
+        categoria: "cozinha",
+        nome: "Porta Guardanapos",
+        valor: 75
+    },
+
+    {
+        categoria: "cozinha",
+        nome: "Porta Talheres",
+        valor: 80
+    },
+
+    {
+        categoria: "cozinha",
+        nome: "Jogo Americano",
+        valor: 85
+    },
+
+    {
+        categoria: "cozinha",
+        nome: "Espremedor de Frutas",
+        valor: 90
+    },
+
+    {
+        categoria: "cozinha",
+        nome: "Kit Utensílios de Silicone",
+        valor: 95
+    },
+
+    {
+        categoria: "cozinha",
+        nome: "Balança de Cozinha",
+        valor: 100
+    },
+
+    {
+        categoria: "cozinha",
+        nome: "Organizador de Temperos",
+        valor: 105
+    },
+
+    {
+        categoria: "cozinha",
+        nome: "Chaleira",
+        valor: 110
+    },
+
+    {
+        categoria: "cozinha",
+        nome: "Kit para Sobremesa",
+        valor: 100
+    },
+
+    {
+        categoria: "cozinha",
+        nome: "Lixeira de Cozinha",
+        valor: 120
+    },
+
+    {
+        categoria: "cozinha",
+        nome: "Potes para Mantimentos",
+        valor: 125
+    },
+
+    {
+        categoria: "cozinha",
+        nome: "Jogo de Panelas",
+        valor: 250
+    },
+
+    {
+        categoria: "cozinha",
+        nome: "Conjunto de Taças",
+        valor: 180
+    },
+
+    {
+        categoria: "cozinha",
+        nome: "Kit Facas",
+        valor: 150
+    },
+
+    {
+        categoria: "cozinha",
+        nome: "Sanduicheira",
+        valor: 130
+    },
+
+    {
+        categoria: "cozinha",
+        nome: "Liquidificador",
+        valor: 140
+    },
+
+    {
+        categoria: "cozinha",
+        nome: "Grill Elétrico",
+        valor: 210
+    },
+
+    {
+        categoria: "cozinha",
+        nome: "Panela Elétrica",
+        valor: 240
+    },
+
+    {
+        categoria: "cozinha",
+        nome: "Air Fryer",
+        valor: 300
+    },
+
+    {
+        categoria: "cozinha",
+        nome: "Micro-ondas",
+        valor: 500
+    },
+
+    {
+        categoria: "cozinha",
+        nome: "Cafeteira Elétrica",
+        valor: 230
+    },
+
+    {
+        categoria: "cozinha",
+        nome: "Aparelho de Jantar",
+        valor: 310
+    },
+
+    {
+        categoria: "cozinha",
+        nome: "Travessas de Vidro",
+        valor: 135
+    },
+
+    {
+        categoria: "cozinha",
+        nome: "Assadeira Antiaderente",
+        valor: 145
+    },
+
+    {
+        categoria: "cozinha",
+        nome: "Tábua de Corte",
+        valor: 65
+    },
+
+    {
+        categoria: "cozinha",
+        nome: "Kit Organizadores de Geladeira",
+        valor: 155
+    },
+
+    {
+        categoria: "cozinha",
+        nome: "Fruteira",
+        valor: 165
+    },
+
+    {
+        categoria: "cozinha",
+        nome: "Escorredor de Louça",
+        valor: 160
+    },
+
+    {
+        categoria: "cozinha",
+        nome: "Torneira Gourmet",
+        valor: 315
+    },
+
+    {
+        categoria: "cozinha",
+        nome: "Jarra de Vidro",
+        valor: 30
+    },
+
+    {
+        categoria: "cozinha",
+        nome: "Conjunto de Xícaras",
+        valor: 170
+    },
+
+    {
+        categoria: "cozinha",
+        nome: "Kit Cozinha Completo",
+        valor: 330
+    },
+
+    {
+        categoria: "cozinha",
+        nome: "Robô Aspirador",
+        valor: 600
+    },
+        // =====================
+    // QUARTO - 30 ITENS
+    // =====================
+
+    {
+        categoria: "quarto",
+        nome: "Travesseiros",
+        valor: 82
+    },
+
+    {
+        categoria: "quarto",
+        nome: "Jogo de Lençol",
+        valor: 127
+    },
+
+    {
+        categoria: "quarto",
+        nome: "Cobertor",
+        valor: 152
+    },
+
+    {
+        categoria: "quarto",
+        nome: "Edredom",
+        valor: 207
+    },
+
+    {
+        categoria: "quarto",
+        nome: "Colcha",
+        valor: 182
+    },
+
+    {
+        categoria: "quarto",
+        nome: "Almofadas",
+        valor: 102
+    },
+
+    {
+        categoria: "quarto",
+        nome: "Cortina para Quarto",
+        valor: 202
+    },
+
+    {
+        categoria: "quarto",
+        nome: "Tapete para Quarto",
+        valor: 157
+    },
+
+    {
+        categoria: "quarto",
+        nome: "Cabides",
+        valor: 62
+    },
+
+    {
+        categoria: "quarto",
+        nome: "Organizadores de Guarda-Roupa",
+        valor: 87
+    },
+
+    {
+        categoria: "quarto",
+        nome: "Manta para Cama",
+        valor: 122
+    },
+
+    {
+        categoria: "quarto",
+        nome: "Abajur",
+        valor: 107
+    },
+
+    {
+        categoria: "quarto",
+        nome: "Luminária",
+        valor: 162
+    },
+
+    {
+        categoria: "quarto",
+        nome: "Espelho",
+        valor: 187
+    },
+
+    {
+        categoria: "quarto",
+        nome: "Cesto Organizador",
+        valor: 92
+    },
+
+    {
+        categoria: "quarto",
+        nome: "Porta Joias",
+        valor: 72
+    },
+
+    {
+        categoria: "quarto",
+        nome: "Relógio",
+        valor: 97
+    },
+
+    {
+        categoria: "quarto",
+        nome: "Criado-Mudo",
+        valor: 252
+    },
+
+    {
+        categoria: "quarto",
+        nome: "Sapateira",
+        valor: 222
+    },
+
+    {
+        categoria: "quarto",
+        nome: "Kit Organizadores",
+        valor: 167
+    },
+
+    {
+        categoria: "quarto",
+        nome: "Aromatizador de Ambiente",
+        valor: 77
+    },
+
+    {
+        categoria: "quarto",
+        nome: "Quadro Decorativo",
+        valor: 112
+    },
+
+    {
+        categoria: "quarto",
+        nome: "Ventilador",
+        valor: 212
+    },
+
+    {
+        categoria: "quarto",
+        nome: "Ferro de Passar",
+        valor: 132
+    },
+
+    {
+        categoria: "quarto",
+        nome: "Tábua de Passar",
+        valor: 172
+    },
+
+    {
+        categoria: "quarto",
+        nome: "Caixa Organizadora Grande",
+        valor: 67
+    },
+
+    {
+        categoria: "quarto",
+        nome: "Roupa de Cama Premium",
+        valor: 307
+    },
+
+    {
+        categoria: "quarto",
+        nome: "Cômoda",
+        valor: 457
+    },
+
+    {
+        categoria: "quarto",
+        nome: "Guarda-Roupa",
+        valor: 800
+    },
+
+    {
+        categoria: "quarto",
+        nome: "Cama Box",
+        valor: 1000
+    },
+    // =====================
+    // SALA - 20 ITENS
+    // =====================
+
+    {
+        categoria: "sala",
+        nome: "Porta Retrato",
+        valor: 54
+    },
+
+    {
+        categoria: "sala",
+        nome: "Vaso Decorativo",
+        valor: 84
+    },
+
+    {
+        categoria: "sala",
+        nome: "Almofadas para Sofá",
+        valor: 108
+    },
+
+    {
+        categoria: "sala",
+        nome: "Plantas",
+        valor: 104
+    },
+
+    {
+        categoria: "sala",
+        nome: "Manta para Sofá",
+        valor: 124
+    },
+
+    {
+        categoria: "sala",
+        nome: "Quadros Decorativos",
+        valor: 154
+    },
+
+    {
+        categoria: "sala",
+        nome: "Bandeja Decorativa",
+        valor: 88
+    },
+
+    {
+        categoria: "sala",
+        nome: "Relógio de Parede",
+        valor: 98
+    },
+
+    {
+        categoria: "sala",
+        nome: "Puff",
+        valor: 184
+    },
+
+    {
+        categoria: "sala",
+        nome: "Kit Decoração Sala",
+        valor: 204
+    },
+
+    {
+        categoria: "sala",
+        nome: "Luminária de Chão",
+        valor: 258
+    },
+
+    {
+        categoria: "sala",
+        nome: "Cortina para Sala",
+        valor: 254
+    },
+
+    {
+        categoria: "sala",
+        nome: "Tapete para Sala",
+        valor: 308
+    },
+
+    {
+        categoria: "sala",
+        nome: "Espelho Grande",
+        valor: 304
+    },
+
+    {
+        categoria: "sala",
+        nome: "Mesa de Centro",
+        valor: 354
+    },
+
+    {
+        categoria: "sala",
+        nome: "Aspirador",
+        valor: 404
+    },
+
+    {
+        categoria: "sala",
+        nome: "Rack para TV",
+        valor: 508
+    },
+
+    {
+        categoria: "sala",
+        nome: "Painel para TV",
+        valor: 608
+    },
+
+    {
+        categoria: "sala",
+        nome: "Poltrona",
+        valor: 504
+    },
+
+    {
+        categoria: "sala",
+        nome: "Sofá",
+        valor: 1204
+    },
+        // =====================
+    // BANHEIRO - 10 ITENS
+    // =====================
+
+    {
+        categoria: "banheiro",
+        nome: "Kit Toalhas",
+        valor: 153
+    },
+
+    {
+        categoria: "banheiro",
+        nome: "Tapete de Banheiro",
+        valor: 66
+    },
+
+    {
+        categoria: "banheiro",
+        nome: "Porta Escovas",
+        valor: 53
+    },
+
+    {
+        categoria: "banheiro",
+        nome: "Dispenser de Sabonete",
+        valor: 63
+    },
+
+    {
+        categoria: "banheiro",
+        nome: "Cesto para Roupa",
+        valor: 136
+    },
+
+    {
+        categoria: "banheiro",
+        nome: "Espelho para Banheiro",
+        valor: 253
+    },
+
+    {
+        categoria: "banheiro",
+        nome: "Organizador de Banheiro",
+        valor: 103
+    },
+
+    {
+        categoria: "banheiro",
+        nome: "Kit Higiene",
+        valor: 123
+    },
+
+    {
+        categoria: "banheiro",
+        nome: "Prateleira para Banheiro",
+        valor: 256
+    },
+
+    {
+        categoria: "banheiro",
+        nome: "Armário para Banheiro",
+        valor: 406
+    }
+
+
+];
